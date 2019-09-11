90f42aa901c3588ea98fbc351f92dc6e32ae7e81
MarijnS95 <marijns95@gmail.com>
2019-09-11 18:16:57 +0200

    WIP: gcc: Build all devices to their own dir.

--- build-kernels-gcc.sh
+++ build-kernels-gcc.sh
@@ -1,7 +1,7 @@
 cd ../../../..
 export ANDROID_ROOT=$(pwd)
-export KERNEL_TOP=$ANDROID_ROOT/kernel/sony/msm-4.9
-export KERNEL_TMP=$ANDROID_ROOT/out/kernel-tmp
+export kernel_top=$ANDROID_ROOT/kernel/sony/msm-4.9
+export kernel_tmp=$ANDROID_ROOT/out/kernel-49
 
 # Cross Compiler
 export CROSS_COMPILE=$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
@@ -9,10 +9,6 @@ export CROSS_COMPILE_ARM32=$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-a
 
 # Mkdtimg tool
 export MKDTIMG=$ANDROID_ROOT/out/host/linux-x86/bin/mkdtimg
-# Build command
-export BUILD="make O=$KERNEL_TMP ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE -j$(nproc)"
-# Copy prebuilt kernel
-export CP_BLOB="cp $KERNEL_TMP/arch/arm64/boot/Image.gz-dtb $KERNEL_TOP/common-kernel/kernel-dtb"
 
 # Check if mkdtimg tool exists
 if [ ! -f $MKDTIMG ]; then
@@ -22,22 +18,23 @@ if [ ! -f $MKDTIMG ]; then
     make mkdtimg
 fi
 
-LOIRE="suzu kugo blanc"
+LOIRE="suzu"
 TONE="dora kagura keyaki"
 YOSHINO="lilac maple poplar"
-NILE="discovery pioneer voyager"
+NILE="discovery"
 GANGES="kirin mermaid"
 TAMA="akari apollo akatsuki"
 
-PLATFORMS="loire tone yoshino nile ganges tama"
+PLATFORMS="loire tone yoshino nile ganges tama kumano"
+PLATFORMS="loire"
 
-cd $KERNEL_TOP/kernel
+cd $kernel_top/kernel
 
 echo "================================================="
 echo "Your Environment:"
 echo "ANDROID_ROOT: ${ANDROID_ROOT}"
-echo "KERNEL_TOP  : ${KERNEL_TOP}"
-echo "KERNEL_TMP  : ${KERNEL_TMP}"
+echo "kernel_top  : ${kernel_top}"
+echo "kernel_tmp  : ${kernel_tmp}"
 
 for platform in $PLATFORMS; do \
 
@@ -63,25 +60,32 @@ tama)
 esac
 
 for device in $DEVICE; do \
-    ret=$(rm -rf ${KERNEL_TMP} 2>&1);
-    ret=$(mkdir -p ${KERNEL_TMP} 2>&1);
-    if [ ! -d ${KERNEL_TMP} ] ; then
+    device_out=$kernel_tmp/${device}-gcc
+    ret=$(mkdir -p $device_out 2>&1);
+    if [ ! -d $device_out ] ; then
         echo "Check your environment";
         echo "ERROR: ${ret}";
         exit 1;
     fi
 
+    # Build command
+    build="make O=$device_out ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE -j$(nproc)"
+
+    # Copy prebuilt kernel
+    CP_BLOB="cp $device_out/arch/arm64/boot/Image.gz-dtb $kernel_top/common-kernel/kernel-dtb"
+
     echo "================================================="
     echo "Platform -> ${platform} :: Device -> $device"
-    ret=$(${BUILD} aosp_$platform"_"$device\_defconfig 2>&1);
+    ret=$(${build} aosp_$platform"_"$device\_defconfig 2>&1);
     case "$ret" in
         *"error"*|*"ERROR"*) echo "ERROR: $ret"; exit 1;;
     esac
 
     echo "The build may take up to 10 minutes. Please be patient ..."
     echo "Building new kernel image ..."
-    echo "Loggin to $KERNEL_TMP/build_log_${device}"
-    $BUILD >$KERNEL_TMP/build_log_${device} 2>&1;
+    build_log="$kernel_tmp/build_log_${platform}_${device}_gcc"
+    echo "Logging to $build_log"
+    $build >$build_log 2>&1;
 
     echo "Copying new kernel image ..."
     ret=$(${CP_BLOB}-${device} 2>&1);
@@ -89,7 +93,7 @@ for device in $DEVICE; do \
         *"error"*|*"ERROR"*) echo "ERROR: $ret"; exit 1;;
     esac
     if [ $DTBO = "true" ]; then
-        $MKDTIMG create $KERNEL_TOP/common-kernel/dtbo-$device\.img `find $KERNEL_TMP/arch/arm64/boot/dts -name "*.dtbo"`
+        $MKDTIMG create $kernel_top/common-kernel/dtbo-$device\.img `find $device_out/arch/arm64/boot/dts -name "*.dtbo"`
     fi
 done
 done
@@ -98,7 +102,4 @@ echo "================================================="
 echo "Clean up environment"
 echo "Done!"
 unset ANDROID_ROOT
-unset KERNEL_TOP
 unset KERNEL_CFG
-unset KERNEL_TMP
-unset BUILD
